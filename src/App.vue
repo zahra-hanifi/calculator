709eae6f9e7f004090a99cabd581c3bb92f8387d
zahra <zahrahanifi98@gmail.com>
2024-04-28 13:59:27 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-gray-100 min-h-screen flex justify-center items-center p-8">
+    <div class="w-full lg:w-1/2 xl:w-1/3 2xl:w-1/3 flex flex-col gap-y-8">
+      <div class="flex justify-center items-center gap-x-1 flex-wrap">
+        <span class="text-gray-600">A Vue calculator Application by</span>
+
+        <a
+          href="https://github.com/zahra-hanifi/"
+          target="_blank"
+          class="font-bold"
+        >
+          Zahra Hanifi
+        </a>
+      </div>
+
+      <div class="bg-white shadow rounded-lg">
+      </div>
+    </div>
+  </div>
+</template>
