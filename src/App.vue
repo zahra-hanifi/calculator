f723f770c694eb2f06a0f3468f1f3fccce1a1212
zahra <zahrahanifi98@gmail.com>
2024-04-28 16:17:40 +0330

    implemented ui of calculator

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <script setup>
+import Calculator from '@/components/CalculatorComponent.vue'
 </script>
 
 <template>
-  <div class="bg-gray-100 min-h-screen flex justify-center items-center p-8">
-    <div class="w-full lg:w-1/2 xl:w-1/3 2xl:w-1/3 flex flex-col gap-y-8">
+  <div class="bg-gray-950 min-h-screen flex justify-center items-center p-8">
+    <div class="w-full md:w-1/2 2xl:w-1/4 flex flex-col gap-y-8">
       <div class="flex justify-center items-center gap-x-1 flex-wrap">
-        <span class="text-gray-600">A Vue calculator Application by</span>
+        <span class="text-gray-300">A Vue calculator Application by</span>
 
-        <a
-          href="https://github.com/zahra-hanifi/"
-          target="_blank"
-          class="font-bold"
-        >
+        <a href="https://github.com/zahra-hanifi/" target="_blank" class="font-bold text-white">
           Zahra Hanifi
         </a>
       </div>
 
-      <div class="bg-white shadow rounded-lg">
+      <div class="bg-[#2a2d2e] shadow rounded-2xl">
+        <Calculator />
       </div>
     </div>
   </div>
---
+++ src/components/CalculatorComponent.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from 'vue'
+
+const input = ref(0)
+const buttons = ref([
+  'c',
+  'bs',
+  '%',
+  '/',
+  '7',
+  '8',
+  '9',
+  'x',
+  '4',
+  '5',
+  '6',
+  '-',
+  '1',
+  '2',
+  '3',
+  '+',
+  '00',
+  '0',
+  '.',
+  '='
+])
+const operands = ref(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '00', '.'])
+
+function handleClick(key) {}
+</script>
+
+<template>
+  <div class="p-6 sm:p-8 flex flex-col gap-y-8">
+    <input
+      v-model="input"
+      class="bg-transparent p-4 outline-0 rounded-3xl text-4xl text-white"
+      dir="rtl"
+    />
+
+    <div class="grid grid-cols-4 gap-6">
+      <button
+        v-for="button in buttons"
+        :key="button"
+        class="flex justify-center items-center w-14 h-14 sm:w-20 sm:h-20 rounded-full bg-[#3a3f42] shadow-md text-lg sm:text-2xl text-red-500 font-medium"
+        :class="{
+          'bg-[#2f3336] text-white': operands.includes(button),
+          'bg-red-500 !text-gray-900': button === '='
+        }"
+        @click="handleClick(button)"
+      >
+        {{ button.toUpperCase() }}
+      </button>
+    </div>
+  </div>
+</template>
