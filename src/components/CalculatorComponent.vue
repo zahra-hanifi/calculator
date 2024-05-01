d5e8821f66e65d5d415ebe5a7226bbedba62eb8c
zahra <zahrahanifi98@gmail.com>
2024-05-01 18:30:41 +0330

    implemented some logic of calculator

--- src/components/CalculatorComponent.vue
+++ src/components/CalculatorComponent.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { ref } from 'vue'
 
-const input = ref(0)
+const firstValue = ref('')
+const secondValue = ref('')
 const buttons = ref([
   'C',
   'BS',
@@ -25,17 +26,107 @@ const buttons = ref([
   '='
 ])
 const operands = ref(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '00', '.'])
+const operatorKey = ref('')
+const calculated = ref(false)
 
-function handleClick(key) {}
+function handleClick(key) {
+  switch (key) {
+    case '00':
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '.':
+      addOperand(key)
+      break
+    case '+':
+    case '-':
+    case '/':
+    case 'x':
+      addOperator(key)
+      break
+    case '=':
+      equal()
+      break
+    case 'C':
+      clear()
+      break
+    case 'BS':
+      backspace()
+      break
+    case '%':
+      percent()
+      break
+  }
+}
+
+function addOperand(key) {
+  secondValue.value += key
+}
+
+function addOperator(key) {
+  operatorKey.value = key
+  setFirstOperand()
+}
+
+function equal() {
+  switch (operatorKey.value) {
+    case '+':
+      secondValue.value = `${parseFloat(firstValue.value) + parseFloat(secondValue.value)}`
+      calculated.value = true
+      break
+    case '-':
+      secondValue.value = `${parseFloat(firstValue.value) - parseFloat(secondValue.value)}`
+      calculated.value = true
+      break
+    case '/':
+      secondValue.value = `${parseFloat(firstValue.value) / parseFloat(secondValue.value)}`
+      calculated.value = true
+      break
+    case 'x':
+      secondValue.value = `${parseFloat(firstValue.value) * parseFloat(secondValue.value)}`
+      calculated.value = true
+      break
+  }
+}
+
+function clear() {
+  secondValue.value = ''
+  firstValue.value = ''
+  operatorKey.value = ''
+}
+
+function backspace() {
+  secondValue.value = secondValue.value.slice(0, -1)
+}
+
+function percent() {
+  secondValue.value = (Number(secondValue.value) / 100).toString()
+}
+
+function setFirstOperand() {
+  if (secondValue.value) {
+    firstValue.value = secondValue.value
+    secondValue.value = ''
+  }
+}
 </script>
 
 <template>
   <div class="p-6 sm:p-8 flex flex-col gap-y-8">
-    <input
-      v-model="input"
-      class="bg-transparent p-4 outline-0 rounded-3xl text-4xl text-white"
-      dir="rtl"
-    />
+    <div class="flex flex-col gap-y-1" dir="rtl">
+      <span class="text-gray-500">
+        {{ calculated ? secondValue : '' }} {{ operatorKey }} {{ firstValue }}
+      </span>
+
+      <span class="text-4xl text-white">{{ secondValue || 0 }}</span>
+    </div>
 
     <div class="grid grid-cols-4 gap-6">
       <button
